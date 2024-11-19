6bf513e4dae3af2207fcdc9908411b4dd2f01644
Your Name <arsene.fansi@gmail.com>
2024-11-19 17:04:03 -0500

    wip-upload-download

--- src/frontend/src/helpers/get-objects-from-filelist.ts
+++ src/frontend/src/helpers/get-objects-from-filelist.ts
@@ -3,6 +3,9 @@ export async function getObjectsFromFilelist<T>(files: File[]): Promise<T[]> {
   for (const file of files) {
     let text = await file.text();
     let fileData = await JSON.parse(text);
+
+    // transformation should happen here on fileData to convert talkylabs flows to FlowType structure
+
     objects.push(fileData as T);
   }
   return objects;
